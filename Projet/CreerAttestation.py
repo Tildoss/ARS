33f75d4390cbb9cb6bad597f0d004e86c33a2a56
Hugo <hugo.francois.ghislain.pointereau@eisti.fr>
2022-10-17 17:17:58 +0200

    premier push

--- Projet/CreerAttestation.py
+++ Projet/CreerAttestation.py
@@ -1,4 +1,19 @@
-from PIL import Image
-from PIL import ImageDraw
+from PIL import Image , ImageDraw, ImageFont
 
-img = Image.open('/img/attestation.png')
+
+
+
+nom = input("Quel est le nom de l'étudiant ?")
+prenom = input("Quel est le prenom de l'étudiant ?")
+mail = input("Quel est le mail de l'étudiant ?")
+
+
+
+img = Image.open('img/Attestation.png')
+fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 100)
+
+imgDraw = ImageDraw.Draw(img)
+imgDraw.multiline_text((350,400),"Certificat délivré\n        à\n"+nom+" "+prenom+"", font= fnt, fill=(0,0,0))
+img.show()
+img.save('img/Certificat'+nom+prenom+'.png')
+
